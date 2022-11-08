552fd81f33ff9a91011f7d50b5929fe62d9dce2f
Tamema-TT <tamema.tama@northsouth.edu>
2022-11-09 02:12:06 +0600

    Complete

--- src/pages/Footer/Footer.js
+++ src/pages/Footer/Footer.js
@@ -11,7 +11,7 @@ const Footer = () => {
                     <Link><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path></svg></Link>
                 </div>
                 <div>
-                    <p>Copyright © 2022 - All right reserved by ACME Industries Ltd</p>
+                    <p>Copyright © 2022 - All right reserved by Jo Chef Ltd</p>
                 </div>
             </footer>
         </div>
